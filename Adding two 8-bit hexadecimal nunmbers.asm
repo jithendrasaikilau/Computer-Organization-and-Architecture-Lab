d0419a2184d07994e635ce4bcc559581d3cd9cc7
JITHENDRASAI.KILARU <147501428+jithendrasaikilau@users.noreply.github.com>
2024-08-23 22:05:51 +0530

    Add files via upload

---
+++ Adding two 8-bit hexadecimal nunmbers.asm
@@ -0,0 +1,34 @@
+;code for adding two 8-bit hexadecimal numbers
+org 100h
+
+num1 db 12h
+num2 db 1Ah
+
+start:
+mov al,num1
+add al,num2
+mov bl, al
+;converting the higher nibble
+mov ah, al
+and ah,0F0h
+shr ah, 4
+add ah, 30h
+cmp ah, 39h
+jle first_bit
+add ah,7
+first_bit: 
+           mov dl,ah
+           mov ah,02h
+           int 21h  
+;converting the lower nibble 
+mov ah,bl
+and ah,0Fh    
+add ah,30h
+cmp ah,39h
+jle second_bit
+add ah,7
+second_bit: 
+            mov dl,ah
+            mov ah,02h
+            int 21h
+ret
